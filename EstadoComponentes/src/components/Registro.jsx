3fec4ada2294adc9da359ec325486009b8438ab2
Jcidcab84 <juancid62@gmail.com>
2024-01-20 20:51:01 -0300

    listo

--- EstadoComponentes/src/components/Registro.jsx
+++ EstadoComponentes/src/components/Registro.jsx
@@ -12,7 +12,7 @@ const Registro = () =>{
             <h1 className="text-primary">Crea una cuenta</h1>
             <div className="d-flex gap-3 justify-content-center my-3"> 
                 <SocialButton icon="fa-brands fa-facebook" link="https://web.facebook.com"/>
-                <SocialButton icon="fa-brands fa-github"link="https://github.com/Jcidcab84/EstadosComponentes"/>
+                <SocialButton icon="fa-brands fa-github"link="https://github.com/Jcidcab84"/>
                 <SocialButton icon="fa-brands fa-linkedin" link="https://www.linkedin.com/in/juan-jos%C3%A9-cid-b739a061/" />
             </div>
             <p className="text-primary">O usa tu email para registrarte</p>
